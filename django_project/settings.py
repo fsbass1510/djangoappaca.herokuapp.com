55e984709280c998ca13b3b933408abea8dff12f
Ian Issachar Hermosilla <ian.issachar.hermosilla@gmail.com>
2020-02-19 03:37:51 +0800

    runtime txt added

--- django_project/settings.py
+++ django_project/settings.py
@@ -145,6 +145,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
